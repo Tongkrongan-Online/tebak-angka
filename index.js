d7e0c7e9e9ddc05e3e16000de84b126f6cd80175
Tongkrongan-Online <mdavafahreza05@gmail.com>
2022-10-16 13:26:06 +0700

    push perbaikan ke-3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 alert(
-  "selamat datang di permaiann game tebak angka yang sangat sederhana memanfaatkan prompt website \n dibuat dengan bahasa pemrograman javascript"
+  "selamat datang di permainan game tebak angka yang sangat sederhana memanfaatkan prompt website \n dibuat dengan bahasa pemrograman javascript \n dibuat oleh \n Muhammad Dava Fahreza"
 );
 
 let ulangGame = true;
